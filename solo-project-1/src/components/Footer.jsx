a48f1a5ea9bf2c989d9fa080d68f0c56c424c473
Yahya <yahyaabbakar@gmail.com>
2023-01-29 23:52:07 +0200

    added and styled about, interests, and footer components

---
+++ solo-project-1/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="socials">
+      <img src="./src/assets/twitter-icon.png" alt="" />
+      <img src="./src/assets/facebook-icon.png" alt="" />
+      <img src="./src/assets/instagram-icon.png" alt="" />
+      <img src="./src/assets/github-icon.png" alt="" />
+    </footer>
+  );
+}

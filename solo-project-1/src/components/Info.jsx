808e869d097a49f5e43f4874cfdca4e299bf7bd7
Yahya <yahyaabbakar@gmail.com>
2023-01-29 18:21:16 +0200

    added info component and styled it

--- solo-project-1/src/components/Info.jsx
+++ solo-project-1/src/components/Info.jsx
@@ -5,10 +5,15 @@ export default function Info() {
       <h1 className="card-name">Laura Smith</h1>
       <h3 className="card-occupation">Frontend Developer</h3>
       <h4 className="card-website">laurasmith.website</h4>
-      <div className="buttons">
-        <button className="email-btn">Email</button>
-        <button className="linkedin-btn">LinkedIn</button>
-      </div>
+      {/* <div className="buttons"> */}
+      <button className="email-btn">
+        <i className="fa-solid fa-envelope"></i>Email
+      </button>
+      <button className="linkedin-btn">
+        <i className="fa-brands fa-linkedin" />
+        LinkedIn
+      </button>
+      {/* </div> */}
     </div>
   );
 }
